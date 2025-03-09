a7d61ab2561b9d3a982350ceba2a00c3f0221d2b
glenn-d-soriano <glenndannielsoriano@gmail.com>
2025-03-08 22:18:44 -0600

    Home Page Assignment

---
+++ week01/scripts/getdate.js
@@ -0,0 +1,5 @@
+// Set current year in footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Set last modified date in footer
+document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
